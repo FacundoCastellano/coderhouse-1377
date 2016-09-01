4c519327858d1c144538763a3ae044a9d5943fa6
FacundoCastellano <fcastellano@est.frba.utn.edu.ar>
2016-08-31 23:51:08 -0300

    TP de clase 1

---
+++ clase-1/TP/js/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Facundo Castellano on 31/8/2016.
+ */
+
+//Pedir un numero, luego otro
+var numero = parseInt(prompt('Ingresar un numero'));
+var otroNumero = parseInt(prompt('Ingresar otro numero'));
+
+//Suma
+alert('La suma de tus numeros es igual a ' + (numero + otroNumero));
+//Resta
+alert('La resta de tus numeros es igual a ' + (numero - otroNumero));
+//Multiplicacion
+alert('La multiplicacion de tus numeros es igual a ' + (numero * otroNumero));
+//Division
+alert('La division de tus numeros es igual a ' + (numero / otroNumero));
